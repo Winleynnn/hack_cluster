f027df5b98a4c6004111d431b15bff4e7bdd5ce9
Konstantin <kostia1500@gmail.com>
2023-05-20 17:08:49 +0300

    v 0.7

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ import os
 from os import walk
 import csv
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+import warnings
+import numpy as np
+warnings.filterwarnings('ignore')
 
 # def get_data_path(data_path = './train_dataset_Росатом/Anonimized', save_path = './train_dataset_Росатом/Anonimized/final_'):
 f = []
@@ -53,42 +59,197 @@ for champ_file in f:
                     p = row[fio_in].split('; ')
                     # print(len(p))
                     # print(p)
+
                     if len(p) == 1 and row[fio_in] != 'XXX':
+
                         # print(competitors_dict[row[fio_in]])
                         competitors_dict[row[fio_in]].append(row[marks_in].replace('%', '').replace(',', '.'))
                         competitors_dict[row[fio_in]].append('s')
+                        competitors_dict[row[fio_in]].append(None)
+                        competitors_dict[row[fio_in]].append(None)
+                        competitors_dict[row[fio_in]].append(rez_exist)
+                        if rez_exist:
+                            competitors_dict[row[fio_in]].append(row[rez_ind])
+                            for i in rez_lst:
+                                competitors_dict[row[fio_in]].append(row[i])
+
                     elif len(p) > 1:
                         # print(p)
                         for i in p:
-                            competitors_dict[i].append((row[marks_in].replace('%', '') if row[marks_in] != '' else row[ed_in]).replace(',', '.'))
+                            competitors_dict[i].append(float((row[marks_in].replace('%', '') if row[marks_in] != '' else row[ed_in]).replace(',', '.'))/len(p))
                             competitors_dict[i].append('t')
                             competitors_dict[i].append(row[1])
                             competitors_dict[i].append(len(p))
+                            competitors_dict[i].append(rez_exist)
+                            if rez_exist:
+                                competitors_dict[i].append(row[rez_ind])
+                                for j in rez_lst:
+                                    competitors_dict[i].append(row[j])
                             # competitors_dict[row[fio_in][i]].append(row[marks_in])
-                    # 334, 342
+
 
                 else:
                     fio_in = row.index('ФИО') if 'ФИО' in row else row.index('ФИО участников')
                     ed_in = row.index('Баллы, ед.')
+                    marks_in = row.index('Баллы, %')
+                    rez_cnt = 1
+                    rez_exist = False
+                    # print(champ_file)
+                    if 'Результат' in row:
+                        rez_ind = row.index("Результат")
+                        rez_exist = True
+                        rez_lst = []
+                        for i in range(rez_cnt, 19): #TODO: Заменить 19 на чиселку n - функция
+                            if (rez_str := f'Результат.{i}') in row:
+                                rez_lst.append(row.index(rez_str))
 
-                    marks_in = row.index(
-                        'Баллы, %')
 
 competitors_dict_filt = {k: v for k, v in competitors_dict.items() if v is not None}
 # competitors_dict_filt = {k: ''.join(v).replace(',', '.') for k, v in competitors_dict.items() if v is not None}
-for key, value in competitors_dict_filt.items():
-    print(key, ' : ', value)
+# for key, value in competitors_dict_filt.items():
+#     print(key, ' : ', value)
 
 
 a = result = [[key] + value for key, value in competitors_dict_filt.items()]
-print(a)
+# print(a)
 df = pd.DataFrame(a)
+df.columns = ['name', 'summary_rez', 'comp_stat', 'team_name', 'team_memb', 'rez_exist', 'rezult', *[f'rezult_{i}' for i in range(1, 18)]]
+# print(df.columns)
+
+# df.to_csv('out.csv')
+
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+df2 = pd.read_csv("train_dataset_Росатом/Участники.csv")
+#меняем значение в перемнной образование по словарю
+# print(df2['Образование'].unique().tolist())
+prof_dict = {np.nan: "Информация об образовании не указана", 'Высшее': "Высшее образование без указания уровня", 'бакалавр': "Бакалавриат",
+             'высшее': "Высшее образование без указания уровня", 'Основное общее образование': "Основное общее образование",
+             'Высшее ': "Высшее образование без указания уровня", 'Среднее профессиональное': "Среднее профессиональное образование",
+             'высшее, специалитет': "Специалитет", 'Неоконченное высшее': "Неоконченное высшее образование",
+             'Среднее специальное ': "Среднее профессиональное образование", '14.05.02': "Специалитет",
+             'Всшее;Среднее специальное': "Среднее профессиональное образование и высшее образование без указания уровня",
+             'Студент ': "Получает высшее образование", 'Среднее-специальное': "Среднее профессиональное образование",
+             'Высшее;Высшее': "Высшее образование без указания уровня", 'Высшее;Среднее техническое': "Среднее профессиональное образование и высшее образование без указания уровня",
+             'Среднее профессиональное;Начальное профессиональное': "Среднее профессиональное образование",
+             'ВЫСШЕЕ ТЕХНИЧЕСКОЕ': "Высшее образование без указания уровня", 'Среднее общее': "Среднее общее образование",
+             'высшее;высшее': "Высшее образование без указания уровня", 'неоконченное СПО': "Неоконченное среднее профессиональное образование",
+             'Полное высшее': "Высшее образование без указания уровня", 'Среднее': "Среднее профессиональное образование",
+             'ВЫСШЕЕ': "Высшее образование без указания уровня", 'высшее профессиональное': "Высшее образование без указания уровня",
+             'Специальное профессиональное': "Среднее профессиональное образование",
+             'Высшее ;Высшее;Среднее специальное ': "Среднее профессиональное образование и высшее образование без указания уровня",
+             'Высшее (Бакалавриат)': "Бакалавриат", 'Среднее специальное': "Среднее профессиональное образование",
+             'профессиональное': "Среднее профессиональное образование", 'Среднеспециальное ': "Среднее профессиональное образование",
+             'Высшее ;Высшее': "Высшее образование", 'Средне-специальное;Высшее': "Среднее профессиональное образование и высшее образование без указания уровня",
+             'Высшее (магистр);Высшее (специалист)': "Магистратура и специалитет", 'среднее профессиональное': "Среднее профессиональное образование"}
+df2['Образование'] = df2['Образование'].replace(prof_dict)
+#конвертируем дату рождения в нужный формат
+df2['Дата рождения'] = pd.to_datetime(df2['Дата рождения'][df2['Дата рождения'].notnull()], errors='coerce')
+#считаем текущий возраст
+df2['Текущий возраст'] = (pd.Timestamp.now() - df2['Дата рождения'][df2['Дата рождения'].notnull()])/np.timedelta64(1, 'Y')
+#избавляемся от некорректных данных по возрасту
+df2["Текущий возраст"] = df2["Текущий возраст"][df2["Текущий возраст"]>14]
+# df_out = pd.read_csv("out.csv", sep=",")
+result1 = pd.concat([df2, df], axis=1)
+#конвертируем возраст в инт, чтобы округлить его
+result1["Текущий возраст"] = result1["Текущий возраст"][result1["Текущий возраст"].notnull()].astype('int')
+#рассчитываем количество интервалов для разбиения на интервалы
+n = 1 + 3.322*np.log10(result1["Текущий возраст"].count())
+n = int(n)
+h = (result1["Текущий возраст"].max() - result1["Текущий возраст"].min())/n
+x = result1["Текущий возраст"].min()
+#создаем словарь с значениями границ интервалов
+intervals = {}
+for i in range(n):
+    first = x
+    second = x + h
+    data = [first, second]
+    intervals[i] = data
+    x = second
+#создаем интервалы и обозначения для них
+bins = np.arange(result1["Текущий возраст"].min(), result1["Текущий возраст"].max()+h, h)
+labels = np.arange(1, len(bins))
+#добавляем метки интервалов возрастов в датасет
+result1["Интервал по возрасту"] = pd.cut(result1["Текущий возраст"], bins=bins,
+                        labels=labels, right=False)
+#удаляем лишние переменные
+# result1.drop(columns=['Unnamed: 0', 'name'], axis=1, inplace=True)
+#сохраняем файл
+filepath = Path('final2_data2.xlsx')
+filepath.parent.mkdir(parents=True, exist_ok=True)
+result1.to_excel(filepath)
 
 
-df.to_csv('out.csv')
+import re
+# df2 = pd.read_excel('final_data.xlsx', index_col=0)
+# # print(df2['Начало трудовой деятельности в РОСАТОМ'])
+# df2 = df2[pd.to_datetime(df2['Начало трудового стажа'], errors='coerce').notnull()]
+# # print(df2)
+# # df[(df == 'banana').any(axis=1)]
+
+
+# df2 = df2[df2['summary_rez'].notna()]
+X = result1[['Текущий возраст', 'summary_rez']]
+X = X.dropna()
+k = 4  # Number of clusters
+kmeans = KMeans(n_clusters=k)
+clusters = kmeans.fit_predict(X)
+# result1= result1.astype({'Results': 'float'}).dtypes
+plt.figure(figsize=(10, 5))
+scatter = plt.scatter(X['Текущий возраст'], X['summary_rez'], c=clusters, cmap='viridis')
+plt.xlabel('Age')
+plt.ylabel('Received Points')
+plt.title('Clustering based on Age and Received Points')
+legend_handles = []
+unique_clusters = np.unique(clusters)
+# print(unique_clusters)
+for cluster_label in unique_clusters:
+    if cluster_label == -1:
+        legend_label = 'Noise'
+        color = scatter.cmap(scatter.norm(cluster_label))
+    else:
+        legend_label = f'Cluster {cluster_label}'
+        color = scatter.cmap(scatter.norm(cluster_label))
+    legend_handles.append(plt.Line2D([0], [0], marker='o', color='w', label=legend_label, markerfacecolor=color, markersize=8))
+#
+plt.legend(handles=legend_handles, title='Clusters')
+plt.colorbar(scatter, label='Cluster')
+plt.show()
+plt.show()
 
 
 
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 
+# Perform clustering using DBSCAN
+eps = 0.5  # Maximum distance between two samples to be considered in the same neighborhood
+min_samples = 2  # Minimum number of samples in a neighborhood for a data point to be considered as a core point
+dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+clusters = dbscan.fit_predict(X_scaled)
 
+# Visualize initial and received values
+plt.figure(figsize=(10, 5))
+scatter = plt.scatter(X['Текущий возраст'], X['summary_rez'], c=clusters, cmap='viridis')
+plt.xlabel('Age')
+plt.ylabel('Received Points')
+plt.title('Clustering based on Age and Received Points (DBSCAN)')
+clusters = dbscan.fit_predict(X_scaled)
+# num_clusters = len(np.unique(clusters))
+legend_handles = []
+unique_clusters = np.unique(clusters)
+# print(unique_clusters)
+for cluster_label in unique_clusters:
+    if cluster_label == -1:
+        legend_label = 'Noise'
+        color = scatter.cmap(scatter.norm(cluster_label))
+    else:
+        legend_label = f'Cluster {cluster_label}'
+        color = scatter.cmap(scatter.norm(cluster_label))
+    legend_handles.append(plt.Line2D([0], [0], marker='o', color='w', label=legend_label, markerfacecolor=color, markersize=8))
 
+plt.legend(handles=legend_handles, title='Clusters')
+plt.colorbar(scatter, label='Cluster')
+plt.show()
